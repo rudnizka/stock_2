7009b96437f50a13b4fc874b9bbd5d375cfe0ea5
jura_k <yurakvlad@gmail.com>
2019-06-09 19:23:22 -0700

    compon API

---
+++ app/src/main/java/com/example/olgarudnytska/stock_2/API.java
@@ -0,0 +1,14 @@
+package com.example.olgarudnytska.stock_2;
+
+public class API {
+    public static String CATEGORIES = "/wp-json/rest/v1/categories";
+    public static String PRODUCTS = "/wp-json/rest/v1/products";
+    public static String DETAIL = "/wp-json/rest/v1/productDetails";
+    public static String MARKER_MAP = "/wp-json/rest/v1/stations";
+    public static String REPAIRS = "/wp-json/rest/v1/services";
+    public static String NEWS = "/wp-json/rest/v1/news";
+    public static String NEWS_DETAIL = "/wp-json/rest/v1/newsDetails";
+    public static String SEND_PRODUCT = "ua/wp-json/rest/v1/buy";
+    public static String SEND_FEEDBACK = "/wp-json/rest/v1/feedbackRequest";
+    public static String SEND_SERVICES = "/wp-json/rest/v1/requestServices";
+}
