257eefdddf0ecd52034086d88bde992f3a87c761
jura_k <yurakvlad@gmail.com>
2019-06-12 10:06:20 -0700

    compon API param +

---
+++ app/src/main/java/com/example/olgarudnytska/stock_2/StockDeclareScreens.java
@@ -0,0 +1,17 @@
+package com.example.olgarudnytska.stock_2;
+
+import com.dpcsa.compon.base.DeclareScreens;
+
+public class StockDeclareScreens extends DeclareScreens {
+    public final static String MAIN = "main", HOME = "home", SERVICE = "service", NEWS = "news",
+            CATEGORY = "category", PRODUCT = "product", ITEM_FORM_REPAIR = "item_form_repair",
+            ITEM_FORM = "item_form", REPAIRS_MAIN = "repairs_main", REPAIRS_CALC = "repairs_calc",
+            ITEM_FORM_SERVICE = "ITEM_FORM_SERVICE", COUNTRY_CODE_PH = "COUNTRY_CODE_PH", COMENT = "COMENT",
+            THANKS = "THANKS", NEWS_DETAIL = "NEWS_DETAIL", ABOUT = "ABOUT", WRITE_US = "WRITE_US",
+            BACK_THANKS = "BACK_THANKS", YOUTUBE = "YOUTUBE";
+
+    @Override
+    public void declare() {
+
+    }
+}
